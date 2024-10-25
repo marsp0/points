f36c7b2188e81873caf5e314cbdac2e7b081723d
marsp0 <martin.n.spasov@gmail.com>
2024-10-25 15:35:26 +0300

    run overbid check for chars not in guild (alts)

--- gargul-overbids.lua
+++ gargul-overbids.lua
@@ -21,6 +21,7 @@ function run_point_check()
 
 
     local current_points = {}
+    local main_alt_map = {}
     local total_points = {}
     local items = {}
 
@@ -37,12 +38,16 @@ function run_point_check()
         end
     end
 
+    -- populate temp cache of alt main map - all lower
+    for k, v in pairs(PTS_alt_main_map) do main_alt_map[string.lower(k)] = string.lower(v) end
+
     -- check for multibids
     for id, auction in pairs(auctions) do
         local current = auction.CurrentBid
         if auction.endsAt ~= 0 and current then
-            total_points[current.player] = total_points[current.player] + current.amount
-            items[current.player] = auction.name .. " " .. items[current.player] 
+            local player_name = main_alt_map[current.player] or current.player
+            total_points[player_name] = total_points[player_name] + current.amount
+            items[player_name] = auction.name .. " " .. items[player_name] 
         end
     end
 
@@ -61,12 +66,13 @@ function run_point_check()
     for id, auction in pairs(auctions) do
         if auction.endsAt ~= 0 and auction.BidsPerPlayer then
             for player, bid in pairs(auction.BidsPerPlayer) do
-                if not last_broadcast_time[player] then last_broadcast_time[player] = 0 end
-                local overbid = current_points[player] < bid
-                local can_broadcast = current_time - last_broadcast_time[player] > broadcast_interval_s
+                local player_name = main_alt_map[player] or player
+                if not last_broadcast_time[player_name] then last_broadcast_time[player_name] = 0 end
+                local overbid = current_points[player_name] < bid
+                local can_broadcast = current_time - last_broadcast_time[player_name] > broadcast_interval_s
                 if overbid and can_broadcast then
-                    broadcast(string.format(overbid_msg, player, bid, current_points[player], auction.name))
-                    last_broadcast_time[player] = current_time
+                    broadcast(string.format(overbid_msg, player_name, bid, current_points[player_name], auction.name))
+                    last_broadcast_time[player_name] = current_time
                 end
             end
         end
